1b8ee5f2983cbda0ee7637cfad49d2acef4b3f3f
Matheus Andrade <matheusvncs160@gmail.com>
2020-07-03 12:09:08 -0300

    Defining the Player movement in GameScene, there is not the AR input yet

--- Rainbrow/GameScene.swift
+++ Rainbrow/GameScene.swift
@@ -11,79 +11,63 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    private var playerNode: Player?
+    var moving: Bool = false
+    
+    var generator: UIImpactFeedbackGenerator!
     
     override func didMove(to view: SKView) {
+        playerNode = self.childNode(withName: "player") as? Player
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        generator = UIImpactFeedbackGenerator(style: .light)
+        generator.prepare()
+        
+    }
+    
+    func updatePlayer(state: PlayerState){
+        if !moving{
+            movePlayer(state: state)
         }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+    }
+    
+    func movePlayer(state: PlayerState){
+        if let player = playerNode{
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+            player.texture = SKTexture(imageNamed: state.rawValue)
+            
+            var direction: CGFloat = 0
+            
+            switch state {
+            case .neutral:
+                direction = 0
+            case .down:
+                direction = -160
+            case .up:
+                direction = 160
+            }
+            
+            if Int(player.position.y) + Int(direction) >= -587 && Int(player.position.y) + Int(direction) <= 373{
+                
+                moving = true
+                let moveAction = SKAction.move(by: CGVector(dx: 0, dy: direction), duration: 0.3)
+                
+                let moveEndedAction = SKAction.run{
+                    self.moving = false
+                    
+                    if direction != 0 {
+                        self.generator.impactOccurred()
+                    }
+                }
+                
+                let sequence = SKAction.sequence([moveAction, moveEndedAction])
+                player.run(sequence)
+                
+            }
         }
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        
     }
+    
 }
---
+++ Rainbrow/Player.swift
@@ -0,0 +1,21 @@
+//
+//  Player.swift
+//  Rainbrow
+//
+//  Created by Matheus Andrade on 03/07/20.
+//  Copyright © 2020 Matheus Andrade. All rights reserved.
+//
+
+import SpriteKit
+
+enum PlayerState: String{
+    
+    case neutral = "Neutral"
+    case down = "Down"
+    case up = "Up"
+    
+}
+
+class Player: SKSpriteNode{
+    
+}
