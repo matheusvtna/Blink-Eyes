2623451caccb21ff18aee3d9c3cd2e5db87f3bff
Matheus Andrade <matheusvncs160@gmail.com>
2020-07-03 16:55:34 -0300

    Insert ARKit and create an ARSession, there's not an update yet

--- Rainbrow/GameViewController.swift
+++ Rainbrow/GameViewController.swift
@@ -8,21 +8,27 @@
 
 import UIKit
 import SpriteKit
-import GameplayKit
+import ARKit
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController, ARSessionDelegate {
 
+    var gameScene: GameScene!
+    var session: ARSession!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
+            if let scene = SKScene(fileNamed: "GameScene") as? GameScene {
                 // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
+                
+                gameScene = scene
+                
+                gameScene.scaleMode = .aspectFill
                 
                 // Present the scene
-                view.presentScene(scene)
+                view.presentScene(gameScene)
             }
             
             view.ignoresSiblingOrder = true
@@ -30,6 +36,35 @@ class GameViewController: UIViewController {
             view.showsFPS = true
             view.showsNodeCount = true
         }
+        
+        session = ARSession()
+        session.delegate = self
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard ARFaceTrackingConfiguration.isSupported else { print("iPhone don't support it"); return }
+        
+        let configuration = ARFaceTrackingConfiguration()
+        
+        session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+        
+    }
+    
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        if let faceAnchor = anchors.first as? ARFaceAnchor{
+            update(withFaceAnchor: faceAnchor)
+        }
+    }
+    
+    func update(withFaceAnchor faceAnchor: ARFaceAnchor){
+        var blendShapes:[ARFaceAnchor.BlendShapeLocation:Any] = faceAnchor.blendShapes
+        
+        
+        
+        
     }
 
     override var shouldAutorotate: Bool {
